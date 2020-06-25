288a7dcb1d8e4b82106f4d1ffeb517d08c1e6640
Evgeny-crypt <Vizor9@bk.ru>
2020-06-25 15:49:51 +1000

    create

---
+++ iters_tz2.py
@@ -0,0 +1,64 @@
+import json
+import requests
+from hashlib import md5
+
+file_patch_read = 'countries.json'
+file_patch_write = 'save_link.txt'
+
+
+def get_country_list(file_patch_read):
+    with open(file_patch_read, encoding='utf8') as file:
+        json_data = json.load(file)
+
+    return [country['name']['common'] for country in json_data]
+
+
+def get_link(search_string, session):
+    params = {'action': 'opensearch',
+              'search': search_string,
+              'limit': '1'}
+
+    response = session.get('https://commons.wikimedia.org/w/api.php', params=params)
+    try:
+        if response.json()[3]:
+            return response.json()[3][0]
+    except json.decoder.JSONDecodeError:
+        return None
+
+
+def write_country(country_data, file_patch_write):
+    with open(file_patch_write, 'a', newline='\n', encoding='utf8') as write_file:
+        write_file.writelines(country_data + '\n')
+
+
+class Wiki_parser:
+    def __init__(self, file_patch_read, file_patch_write):
+        self.country_list = get_country_list(file_patch_read)
+        self.session = requests.Session()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.country_list:
+            country = self.country_list.pop(0)
+            country_data = f'{country} --> {get_link(country, self.session)}'
+            write_country(country_data, file_patch_write)
+            return country_data
+        else:
+            raise StopIteration
+
+
+def json_read_hash(file_patch):
+    with open(file_patch, encoding='utf8') as file:
+        for line in file:
+            yield md5(line.encode()).hexdigest()
+
+
+if __name__ == '__main__':
+
+    for data in Wiki_parser(file_patch_read, file_patch_write):
+        print(data)
+
+    for hashed in json_read_hash('save_link.txt'):
+        print(hashed)
